bd3c706e015872369dc45fc23d0ee570c9215d78
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-06-28 14:55:33 +0200

    removed service part, not applicable

--- get_docker.sh
+++ get_docker.sh
@@ -4,10 +4,8 @@ DOCKER_VER=1.10.3
 echo '----> Pulling and installing docker'
 wget https://downloads.hypriot.com/docker-hypriot_${DOCKER_VER}-1_armhf.deb
 dpkg -i docker-hypriot_*.deb
+rm docker-hypriot_*.deb
 
 echo '----> Adding the user pi to docker group'
 groupadd docker || true
 gpasswd -a pi docker
-
-echo '----> Restarting docker service'
-service docker restart
