649bbbf76337e836febcd69835586305edca2754
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-06-28 14:36:08 +0200

    removed faulty sudo

--- get_docker.sh
+++ get_docker.sh
@@ -3,11 +3,11 @@ DOCKER_VER=1.10.3
 
 echo '----> Pulling and installing docker'
 wget https://downloads.hypriot.com/docker-hypriot_${DOCKER_VER}-1_armhf.deb
-sudo dpkg -i docker-hypriot_*.deb
+dpkg -i docker-hypriot_*.deb
 
 echo '----> Adding the user pi to docker group'
-sudo groupadd docker || true
-sudo gpasswd -a pi docker
+groupadd docker || true
+gpasswd -a pi docker
 
 echo '----> Restarting docker service'
-sudo service docker restart
+service docker restart
